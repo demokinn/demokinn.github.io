498b7aeae2727601b28fc4010e472679bbd4c8ae
demokinn <114749036+demokinn@users.noreply.github.com>
2022-10-02 22:33:13 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,9 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    let targetImage = document.querySelector("#smart-image");
-
-targetImage.addEventListener("click", function() {
-    alert("点击了图片");
-});
-targetImage.classList.add("small");
-targetImage.classList.remove("small");
-});
+    // 你的所有代码都应该写在这里
+    alert("Hello JavaScript");
+})
