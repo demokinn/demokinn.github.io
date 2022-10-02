bd424c0eddf9cf24fbfe0fabbd58d8a8103a73f8
demokinn <114749036+demokinn@users.noreply.github.com>
2022-10-03 00:08:25 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   
 	let targetImage = document.querySelector("#smart-image");	
-	targetImage.addEventListener("click", function() {
- 	   alert("点击了图片");
-});
-
-targetImage.classList.remove("small");
-})
+	document.addEventListener("DOMContentLoaded", function(event) {
+  
+ 
+	let targetImage = document.querySelector("#smart-image");	
+		targetImage.addEventListener("click", function() {
+		    targetImage.classList.remove("small");
+		});
+	});
